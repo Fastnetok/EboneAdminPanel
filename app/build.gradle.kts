4bc63da46bf2e8fc3a2da63657e8fe929f199376
Abbas <ebonebaoo928@gmail.com>
2026-08-02 09:31:47 +0500

    Online billing and payment notification

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -39,8 +39,8 @@ android {
         minSdk = 24
         targetSdk = 36
 
-        versionCode = 24
-        versionName = "1.0.24"
+        versionCode = 25
+        versionName = "1.0.25"
 
         testInstrumentationRunner =
             "androidx.test.runner.AndroidJUnitRunner"
@@ -176,4 +176,4 @@ dependencies {
         libs.androidx.compose.ui.tooling
     )
 }
-// test
+// test1
